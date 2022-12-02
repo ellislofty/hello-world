06860210ab61d4b9b984521844458a43b3f9f6cc
ellislofty <117546953+ellislofty@users.noreply.github.com>
2022-12-02 18:23:43 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
+game.setDialogTextColor(9)
+game.setDialogFrame(img`
+    ..bb9bb99bb99bb99bbbbb..
+    .bddb9ddb9ddb9ddb9bbddb.
+    9ddddb9ddb9ddb9ddb9dddd9
+    9ddddbb99bb99bb99bbdddd9
+    9bddbffffffffffffffbddb9
+    bb9bffffffffffffffffbb9b
+    b9bbffffffffffffffffb9db
+    9bd9ffffffffffffffff9dd9
+    9dd9ffffffffffffffff9db9
+    bd9bffffffffffffffffbb9b
+    b9bbffffffffffffffffb9db
+    9bd9ffffffffffffffff9dd9
+    9dd9ffffffffffffffff9db9
+    bd9bffffffffffffffffbb9b
+    b9bbffffffffffffffffb9db
+    9bd9ffffffffffffffff9dd9
+    9dd9ffffffffffffffff9db9
+    bd9bffffffffffffffffbb9b
+    b9bbffffffffffffffffb9bb
+    9bddbffffffffffffffbddb9
+    9ddddbb99bb99bb99bbdddd9
+    9dddd9bdd9bdd9bdd9bdddd9
+    .9ddbb9bdd9bdd9bdd9bdd9.
+    ..999bb99bb99bb99bb999..
+    `)
+game.splash("Space Hero")
+game.showLongText("Destroy the UFOs and save The Universe", DialogLayout.Bottom)
 scene.setBackgroundImage(img`
     ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
     fffffffffffffffffffffff4fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff4ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
