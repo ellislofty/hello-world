9287bba7407b3a65d75062900b209d51b71f8da1
ellislofty <117546953+ellislofty@users.noreply.github.com>
2022-12-02 20:41:41 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(assets.image`myImage`, mySprite, 0, -100)
 })
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    mySprite2.destroy()
+})
+let projectile2: Sprite = null
+let mySprite2: Sprite = null
 let projectile: Sprite = null
 let mySprite: Sprite = null
 game.setDialogTextColor(9)
@@ -34,22 +39,62 @@ game.splash("Space Hero")
 game.showLongText("Destroy the UFOs and save The Universe", DialogLayout.Bottom)
 effects.starField.startScreenEffect()
 mySprite = sprites.create(img`
-    ....ffffff.........ccc..
-    ....ff22ccf.......cc4f..
-    .....ffccccfff...cc44f..
-    ....cc24442222cccc442f..
-    ...c9b4422222222cc422f..
-    ..c999b2222222222222fc..
-    .c2b99111b222222222c22c.
-    c222b111992222ccccccc22f
-    f222222222222c222ccfffff
-    .f2222222222442222f.....
-    ..ff2222222cf442222f....
-    ....ffffffffff442222c...
-    .........f2cfffc2222c...
-    .........fcc2ffffffff...
-    ..........fc2ffff.......
-    ...........fffff........
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 7 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 7 . . . . . . . 
+    . . . . . . 8 8 5 6 . . . . . . 
+    . . . . . . 8 7 5 6 . . . . . . 
+    . . . . . c c c 6 6 6 . . . . . 
+    . . . . 8 8 7 7 7 5 6 6 . . . . 
+    . . 8 f f f c c 6 6 f f 6 6 . . 
+    . 8 8 8 8 6 6 7 7 7 7 5 7 6 6 . 
+    8 8 8 8 8 8 6 6 7 7 7 5 7 7 6 6 
+    8 8 8 8 8 8 6 6 7 7 7 7 5 7 6 6 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
+mySprite.setPosition(70, 120)
+controller.moveSprite(mySprite, 100, 0)
 mySprite.setStayInScreen(true)
+game.onUpdateInterval(5000, function () {
+    mySprite2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . f a a a a a f . . . . . . 
+        . . f a a a a a a a f . . . . . 
+        . 9 a a a a a a a a a 9 . . . . 
+        . . 9 7 a 7 a 7 a 7 9 . . . . . 
+        . . . 9 9 9 9 9 9 9 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    projectile2 = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 2 1 2 . . . . . . 
+        . . . . . . . 2 1 2 . . . . . . 
+        . . . . . . . 2 1 2 . . . . . . 
+        . . . . . . . 3 1 3 . . . . . . 
+        . . . . . . 2 3 1 3 2 . . . . . 
+        . . . . . . 2 1 1 1 2 . . . . . 
+        . . . . . . 2 1 1 1 3 . . . . . 
+        . . . . . . 3 1 1 1 3 . . . . . 
+        . . . . . . 3 1 1 1 3 . . . . . 
+        . . . . . . 3 1 1 1 3 . . . . . 
+        . . . . . . 2 3 1 3 2 . . . . . 
+        . . . . . . . 2 2 2 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite2, 0, 100)
+    mySprite2.setPosition(70, 0)
+})
